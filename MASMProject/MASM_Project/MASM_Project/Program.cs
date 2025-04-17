c061269e11eeca33db54de24041c7ee5adbf3553
ByGraceMadeNew <krutland@augusta.edu>
2025-04-17 09:26:53 -0400

    Update Program.cs

--- MASMProject/MASM_Project/MASM_Project/Program.cs
+++ MASMProject/MASM_Project/MASM_Project/Program.cs
@@ -3,15 +3,34 @@ class Fibonacci
 {
     static int FibonacciRecursive(int n, int[] fibArray)
     {
-        // logic goes here
+        if (n == 0)
+        {
+            fibArray[0] = 0;
+            return 0;
+        }
+        else if (n == 1)
+        {
+            fibArray[0] = 0;
+            fibArray[1] = 1;
+            return 1;
+        }
+        else if (n>2)
+        {
+            fibArray[n] = FibonacciRecursive(n-2, int[] fibArray) + FibonacciRecursive(n-1, int[] fibArray);
+            return fibArray[n];
+        }
+        else
+        {
+            
+        }
     }
     static void Main()
     {
         int n = 10; // Example input
         int[] fibArray = new int[n + 1];
-        Console.WriteLine(" Fibonacci ␣ of ␣ " + n + " ␣ is : ␣ " +
+        Console.WriteLine(" Fibonacci of " + n + " is: " +
         FibonacciRecursive(n, fibArray));
-        Console.WriteLine(" Fibonacci ␣ Series ␣ up ␣ to ␣ " + n + " : ␣ ");
+        Console.WriteLine(" Fibonacci Series up to " + n + ": ");
         for (int i = 0; i <= n; i++)
         {
             Console.Write(fibArray[i] + " ␣ ");
